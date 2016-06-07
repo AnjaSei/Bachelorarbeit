76b130bd157075d8e1c35c9f958c289629a6dde9
AnjaSei <anjaseidel1@gmx.net>
2016-06-07 21:06:02 +0200

    finished haar wavelet transformation

---
+++ distances.R
@@ -0,0 +1,214 @@
+#!/usr/bin/env Rscript
+
+#top10 rausziehen + "Referenzgen"
+#fuer jedes Gen durchfuehren
+
+########################################################################
+##
+## File: distances.R
+##
+## Created by: Anja S.
+## Created on: June 06, 2016
+##
+## Description: 
+##     
+## Input: 
+##
+## Output: 
+##          
+########################################################################
+
+#install CRAN packages
+#install.packages("optparse")
+#install.packages("infotheo")
+#install.packages("bigmemory")
+
+#load packages
+library(optparse) #to parse command line options
+library(infotheo) #to calculate the mutual information
+library(bigmemory) 
+
+
+#command line options
+option_list <- list(
+  make_option(c("-i", "--input"), action="store", help=".dat file with haar wavelet transformed gene expression data from the Allen Brain Atlas"),
+  make_option(c("-o","--output"), action="store", help="result folder (voluntary)"),
+  
+  #choose a measurement to compare haar wavelet transformed gene expression profiles
+  make_option(c("-e","--euclidean"), action="store_true", default=FALSE, help="Compare haar wavelet transformed gene expression profiles with euclidean distance"),
+  make_option(c("-s","--spearman"), action="store_true", default=FALSE, help="Compare haar wavelet transformed gene expression profiles with spearman correlation coefficient"),
+  make_option(c("-p","--pearson"), action="store_true", default=FALSE, help="Compare haar wavelet transformed gene expression profiles with pearson correlation coefficient"),
+  make_option(c("-r","--rio"), action="store_true", default=FALSE, help="Compare haar wavelet transformed gene expression profiles with RIO (Relative intensity overlap)"),
+  make_option(c("-m","--mutual_information"), action="store_true", default=FALSE, help="Compare haar wavelet transformed gene expression profiles with mutual information")
+  
+)
+
+#parse command line options
+opt <- parse_args(OptionParser(option_list=option_list))
+
+#check if file with gene expression data is given
+if (is.null(opt$input)) {
+  stop("No file with haar wavelet transformed gene expression data is given!")
+}
+
+#check if a measurement (to compare haar wavelet transformed gene expression profiles) has been selected
+if(!any(opt$euclidean, opt$spearman, opt$pearson, opt$rio, opt$mutual_information)){
+  stop("Select a measurement to compare haar wavelet transformed gene expression profiles!")
+}
+#allow only one measurement method
+if(sum(c(opt$euclidean, opt$spearman, opt$pearson, opt$rio, opt$mutual_information))>1){
+  stop("Choose only ONE measurement to compare haar wavelet transformed gene expression profiles!")
+}
+
+#user specified an output/result folder
+if (!is.null(opt$output)) {
+  output_folder<-opt$output
+  
+}else{ #standard output folder
+  output_folder<-"output"
+}
+
+#create output folder
+dir.create(path=output_folder, recursive=TRUE)
+
+
+########################################Functions###############################################
+
+comp_euclidean<-function(data_transformed, number_genes){
+  
+  distance_array<-numeric(length=number_genes*(number_genes-1)/2)
+  
+  pos=1
+  for (reference_gene in 1:(number_genes-1)){
+    for(gene in (reference_gene+1):number_genes){
+      distance_array[pos]<-sqrt(sum( (data_transformed[reference_gene,][-1] - data_transformed[gene,][-1])^2 ))
+      pos=pos+1
+      
+    }
+  }
+  
+  return(distance_array)
+  
+}
+
+
+comp_spearman<-function(data_transformed, number_genes){
+  
+  distance_array<-numeric(length=number_genes*(number_genes-1)/2)
+  
+  pos=1
+  for (reference_gene in 1:(number_genes-1)){
+    for(gene in (reference_gene+1):number_genes){
+      distance_array[pos]<-cor(data_transformed[reference_gene,][-1], data_transformed[gene,][-1], method="spearman")
+      pos=pos+1
+      
+    }
+  }
+  
+  return(distance_array)
+  
+}
+
+comp_pearson<-function(data_transformed, number_genes){
+  
+  distance_array<-numeric(length=number_genes*(number_genes-1)/2)
+  
+  pos=1
+  for (reference_gene in 1:(number_genes-1)){
+    for(gene in (reference_gene+1):number_genes){
+      distance_array[pos]<-cor(data_transformed[reference_gene,][-1], data_transformed[gene,][-1], method="pearson")
+      pos=pos+1
+      
+    }
+  }
+  
+  return(distance_array)
+  
+}
+
+
+comp_rio<-function(data_transformed, number_genes){
+  
+  distance_array<-numeric(length=number_genes*(number_genes-1)/2)
+  
+  pos=1
+  for (reference_gene in 1:(number_genes-1)){
+    for(gene in (reference_gene+1):number_genes){
+      distance_array[pos]<-rio(data_transformed[reference_gene,][-1], data_transformed[gene,][-1])
+      pos=pos+1
+      
+    }
+  }
+  
+  return(distance_array)
+  
+}
+
+
+rio<-function(a, b){
+  rio_ab<-sum(a*b)/sum( ( max(abs(a),abs(b)) )^2 )
+    return(rio_ab)
+}
+
+comp_mutual_information<-function(data_transformed, number_genes){
+  
+  distance_array<-numeric(length=number_genes*(number_genes-1)/2)
+  
+  pos=1
+  for (reference_gene in 1:(number_genes-1)){
+    for(gene in (reference_gene+1):number_genes){
+      distance_array[pos]<-mutinformation(data_transformed[reference_gene,][-1], data_transformed[gene,][-1], method="emp")
+      pos=pos+1
+      
+    }
+  }
+  
+  return(distance_array)
+  
+}
+
+
+################################################################################################
+
+
+##read haar wavelet transformed data
+#file with the gene expression data
+inputfile=opt$input
+#inputfile<-"/home/anja/Schreibtisch/Daten/final/output/haar_wavelet_transformed_data_cube_edge_length_16.txt"
+data_transformed<-read.big.matrix(inputfile, header=TRUE, sep="\t", type="double")
+
+#how to compare gene expression profiles
+distance_measures<-names(which(opt==TRUE))
+number_genes<-nrow(data_transformed)
+#number_genes<-10
+
+
+for (method in distance_measures){
+  
+  switch(method, 
+         euclidean={
+           distance_array<-comp_euclidean(data_transformed, number_genes)
+           write.table(t(round(distance_array, digits = 8)), col.names=FALSE, sep="\t", append=FALSE, row.names=FALSE, file=paste0(output_folder,strsplit(inputfile, ".txt"),"distance_array_",method,".txt"))
+           
+         },
+         spearman={
+           distance_array<-comp_spearman(data_transformed, number_genes)   
+           write.table(t(round(distance_array, digits = 8)), col.names=FALSE, sep="\t", append=TRUE, row.names=FALSE, file=paste0(output_folder,strsplit(inputfile, ".txt"),"distance_array_",method,".txt"))
+           
+           
+         },
+         pearson={
+           distance_array<-comp_pearson(data_transformed, number_genes)    
+           write.table(t(round(distance_array, digits = 8)), col.names=FALSE, sep="\t", append=TRUE, row.names=FALSE, file=paste0(output_folder,strsplit(inputfile, ".txt"),"distance_array_", method, ".txt"))
+         },
+         rio={
+           distance_array<-comp_rio(data_transformed, number_genes)   
+           write.table(t(round(distance_array, digits = 8)), col.names=FALSE, sep="\t", append=TRUE, row.names=FALSE, file=paste0(output_folder,strsplit(inputfile, ".txt"),"distance_array_", method, ".txt"))
+           
+         },
+         mutual_information={
+           distance_array<-comp_mutual_information(round(data_transformed), number_genes) #Werte mussten gerundet werden?   
+           write.table(t(round(distance_array, digits = 8)), col.names=FALSE, sep="\t", append=TRUE, row.names=FALSE, file=paste0(output_folder,strsplit(inputfile, ".txt"),"distance_array_", method, ".txt"))
+         }
+  )
+}
---
+++ haar_wavelet.R
@@ -0,0 +1,223 @@
+#!/usr/bin/env Rscript
+
+########################################################################
+##
+## File: haar_wavelet.R
+##
+## Created by: Anja S.
+## Created on: May 30, 2016
+##
+## Description: Performes Haar wavelet transformation on gene 
+##  expression data from the Allen Brain Atlas
+##     
+## Input: .dat with gene expression data
+##  Path to output folder (optional)
+##  Cube edge length (optional)
+##
+## Output: .txt with Haar wavelet transformed gene expression data
+##          
+########################################################################
+
+#install CRAN packages
+#install.packages("waveslim")
+#install.packages("optparse")
+#install.packages("schoolmath")
+
+#load packages
+library(waveslim) #haar wavelet transformation
+library(optparse) #to parse command line options
+library(schoolmath)
+
+
+#command line options
+option_list <- list(
+  make_option(c("-i", "--input"), action="store", help=".dat file with gene expression data from the Allen Brain Atlas"),
+  make_option(c("-o","--output"), action="store", help="result folder (voluntary)"),
+  make_option(c("-c","--cube_edge_length"), action="store", type="integer", help="user specified cube  edge length; must be a multiple of 2 (voluntary)")
+)
+
+#parse command line options
+opt <- parse_args(OptionParser(option_list=option_list))
+
+#check if file with gene expression data is given
+if (is.null(opt$input)) {
+  stop("No file with gene expression data is given!")
+}
+
+#if cube edge length was specified by the user: check if it is a multiple of 2
+if(!is.null(opt$cube_edge_length)){
+  if(is.decimal(log2(opt$cube_edge_length))){
+    stop("Cube edge length have to be a multiple of 2!")
+  }
+}
+
+#user specified an output/result folder
+if (!is.null(opt$output)) {
+  output_folder<-opt$output
+  
+}else{ #standard output folder
+    output_folder<-"output"
+}
+
+#create output folder
+dir.create(path=output_folder, recursive=TRUE)
+
+
+
+##########Functions##########
+
+#fill given gene expression data (saved in intensities) from one gene centrally into a 3D array (brain)
+fill_brain<-function(intensities, coordinates, cube_edge_length, x_shift, y_shift, z_shift, brain){
+  
+  #position in intensities
+  pos=1
+  
+  #Insert intensities of the "old" cuboid as centrally as possible in the "new" cube, move coordinates accordingly
+  for (coord in 1:ncol(coordinates)){
+    
+    #move data points to the center of the "new cube"
+    x<-coordinates[1,coord]+x_shift
+    y<-coordinates[2,coord]+y_shift
+    z<-coordinates[3,coord]+z_shift
+    
+    #coordinates are not cut off (can happen when the user selects the cube edge length )
+    if(all(c(x,y,z)>=1) && all(c(x,y,z)<=cube_edge_length) ){
+      
+      #save intensities from 1D array in 3D array (cube)
+      brain[x,y,z]<-intensities[pos]
+      pos<-pos+1
+    }
+  }
+  
+  return(brain)
+  
+}
+
+
+
+#############################
+
+#file with the gene expression data
+inputfile=opt$input
+
+#open a connection
+connection<-file(inputfile)
+open(connection)
+
+#count number of lines of the gene expression file
+com<-paste0("wc -l ", inputfile, " | awk '{ print $1 }'")
+row_number<-as.numeric(system(command=com, intern=TRUE))
+
+#start stopwatch
+#ptm <- proc.time()
+
+##save coordinates  of the gene expression data in a matrix 
+#(first row -> x-coordinates, second row -> y-coordinates, third row -> z-coordinates)
+headline <- scan(file=connection, nlines=1,  what=character(), quiet=TRUE, sep="\t")
+#split each coordinate to x-, y- and z-value
+coordinates<-strsplit(headline, split=",")[-1]
+#convert from character to numeric
+coordinates<-sapply(coordinates, as.numeric)
+
+
+#user specified cube edge length
+if(!is.null(opt$cube_edge_length)){
+  cube_edge_length<-opt$cube_edge_length
+  
+} else { #calculate cube edge length
+  
+  #calculate range of the x-, y- resp. z- coordinates
+  #it is  okay to consider only the first and the last coordinate because only whole data planes 
+  #(with only zero-intensities) and no single coordinates are missing in the gene expression data set
+  
+  range_x<-coordinates[1, ncol(coordinates)]-coordinates[1,1]+1 #==range_y<-max(coordinates[1,])-min(coordinates[1,])+1
+  range_y<-coordinates[2, ncol(coordinates)]-coordinates[2,1]+1 #==range_y<-max(coordinates[2,])-min(coordinates[2,])+1
+  range_z<-coordinates[3, ncol(coordinates)]-coordinates[3,1]+1 #==range_z<-max(coordinates[3,])-min(coordinates[3,])+1
+  
+  cube_edge_length<-max(range_x,range_y, range_z)
+  
+  #cube_edge_length is no multiple of 2
+  if(is.decimal(log2(cube_edge_length))){
+    
+    #widen cube_edge_length to a multiple of 2
+    cube_edge_length<-2^ceiling(log2(cube_edge_length))
+    
+  }
+}
+
+#number of decompositions of the haar wavelet transformation
+decomposition_depth=log2(cube_edge_length)
+
+#center of the given gene expression coordinates, considered as a cuboid
+#(min_x+max_x)/2;  m_x_old=(min(coordinates[1,])+max(coordinates[1,]))/2
+m_x_old<-(coordinates[1,1]+coordinates[1,ncol(coordinates)])/2
+#(min_y+max_y)/2;  m_y_old=(min(coordinates[2,])+max(coordinates[2,]))/2
+m_y_old<-(coordinates[2,1]+coordinates[2,ncol(coordinates)])/2
+#(min_z+max_z)/2;  m_z_old=(min(coordinates[3,])+max(coordinates[3,]))/2
+m_z_old<-(coordinates[3,1]+coordinates[3,ncol(coordinates)])/2
+
+#center of the "new" cube (->brain)
+m_x=m_y=m_z=cube_edge_length/2
+
+#shift data points of the "old" cuboid along the x-, y- or z-axis
+x_shift<-ceiling(m_x-m_x_old)
+y_shift<-ceiling(m_y-m_y_old)
+z_shift<-ceiling(m_z-m_z_old)
+
+#"empty" 3D brain (filled with nulls)
+brain<-array(data=0, dim=c(cube_edge_length, cube_edge_length, cube_edge_length))
+
+
+#name of the result file (stores haar wavelet transformed expression data)
+resultfile=paste0(output_folder,"/haar_wavelet_transformed_data_cube_edge_length_",cube_edge_length,".txt")
+
+#build a line for the column names of the resultfile
+column_names<-c("GENEID")
+filter<-c("HLL","LHL","LLH","HHL","HLH","LHH","HHH")
+data_points<-cube_edge_length
+decomposition_step=1
+
+repeat{
+  data_points<-data_points/2
+  for (filtertyp in filter){
+    column_names<-c(column_names, rep(paste0(filtertyp, as.character(decomposition_step)), data_points^3))
+  }
+  if(data_points==1){
+    column_names<-c(column_names, paste0("LLL",decomposition_step))
+    break
+  }
+  decomposition_step<-decomposition_step+1
+  
+}
+write.table(t(column_names), col.names=FALSE, sep="\t", row.names=FALSE, file=resultfile)
+
+
+#for each gene do:
+for(gene in 1:(row_number-1)) {
+
+  #read gene_id and intensities for each coordinate of the gene
+  data <- scan(file=connection, nlines=1,  what=numeric(), quiet=TRUE, sep="\t")
+  
+  #select geneID
+  gene_id<-data[1]
+  
+  #intensities for one gene at each coordinate (without gen_id), planes with only zero-intensities are missing 
+  intensities<-data[-1]
+  
+  #create 3D brain (cube) with the intensities of the gene at the specific coordinates
+  brain<-fill_brain(intensities, coordinates, cube_edge_length, x_shift, y_shift, z_shift, brain)
+  
+  #apply haar wavelet transformation
+  data_transform<-dwt.3d(brain, wf="haar", J=decomposition_depth)
+  
+  #concatenate the data_transform-list to a vector
+  data_transform_vector<-Reduce(c,data_transform)
+  
+  #write transformed gene intensity values to the result file
+  write.table(t(c(gene_id,round(data_transform_vector, digits = 8))), col.names=FALSE, sep="\t", append=TRUE, row.names=FALSE, file=resultfile)
+
+}
+close(connection)
+
+#stop stopwatch
+#proc.time() - ptm
