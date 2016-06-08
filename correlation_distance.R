7c28f582b2a880fd15abd30e44e74213a490259e
AnjaSei <anjaseidel1@gmx.net>
2016-06-08 16:25:22 +0200

    update

--- correlation_distance.R
+++ correlation_distance.R
@@ -8,12 +8,12 @@
 ## Created on: June 06, 2016
 ##
 ## Description:   Calculates correlation or distance between the 
-##  haar wavelet transformed gene expression profiles for each gene pair.
+##  (haar wavelet transformed) gene expression profiles for each gene pair.
 ##     
-## Input:   File with haar wavelet transformed gene expression profiles.
+## Input:   File with (haar wavelet transformed) gene expression profiles.
 ##
 ## Output: File with GeneIDs (first line) and the upper triangle of the 
-##  distance/correlation matrix (second line)
+##  distance/correlation matrix (second line).
 ##          
 ########################################################################
 
@@ -29,15 +29,15 @@ library(data.table) #fast way to load the data
 
 #command line options
 option_list <- list(
-  make_option(c("-i", "--input"), action="store", help=".dat file with haar wavelet transformed gene expression data from the Allen Brain Atlas"),
+  make_option(c("-i", "--input"), action="store", help=".dat file with (e.g. haar wavelet transformed) gene expression data from the Allen Brain Atlas"),
   make_option(c("-o","--output"), action="store", help="result folder (voluntary)"),
   
-  #choose a measurement to compare haar wavelet transformed gene expression profiles
-  make_option(c("-e","--euclidean"), action="store_true", default=FALSE, help="Compare haar wavelet transformed gene expression profiles with euclidean distance"),
-  make_option(c("-s","--spearman"), action="store_true", default=FALSE, help="Compare haar wavelet transformed gene expression profiles with spearman correlation coefficient"),
-  make_option(c("-p","--pearson"), action="store_true", default=FALSE, help="Compare haar wavelet transformed gene expression profiles with pearson correlation coefficient"),
-  make_option(c("-r","--rio"), action="store_true", default=FALSE, help="Compare haar wavelet transformed gene expression profiles with RIO (Relative intensity overlap)"),
-  make_option(c("-m","--mutual_information"), action="store_true", default=FALSE, help="Compare haar wavelet transformed gene expression profiles with mutual information")
+  #choose a measurement to compare (haar wavelet transformed) gene expression profiles
+  make_option(c("-e","--euclidean"), action="store_true", default=FALSE, help="Compare gene expression profiles with euclidean distance"),
+  make_option(c("-s","--spearman"), action="store_true", default=FALSE, help="Compare gene expression profiles with spearman correlation coefficient"),
+  make_option(c("-p","--pearson"), action="store_true", default=FALSE, help="Compare gene expression profiles with pearson correlation coefficient"),
+  make_option(c("-r","--rio"), action="store_true", default=FALSE, help="Compare gene expression profiles with RIO (Relative intensity overlap)"),
+  make_option(c("-m","--mutual_information"), action="store_true", default=FALSE, help="Compare gene expression profiles with mutual information")
   
 )
 
@@ -46,12 +46,12 @@ opt <- parse_args(OptionParser(option_list=option_list))
 
 #check if file with the gene expression data is given
 if (is.null(opt$input)) {
-  stop("No file with haar wavelet transformed gene expression data is given!")
+  stop("No file with (haar wavelet transformed) gene expression data is given!")
 }
 
-#check if at least one measurement (to compare haar wavelet transformed gene expression profiles) has been selected
+#check if at least one measurement (to compare gene expression profiles) has been selected
 if(!any(opt$euclidean, opt$spearman, opt$pearson, opt$rio, opt$mutual_information)){
-  stop("Select a measurement to compare haar wavelet transformed gene expression profiles!")
+  stop("Select a measurement to compare gene expression profiles!")
 }
 
 #user specified an output/result folder
@@ -68,8 +68,8 @@ dir.create(path=output_folder, recursive=TRUE)
 
 ########################################Functions###############################################
 
-#computes euclidean distance between transformed gene expression profiles for each  gene pair
-comp_euclidean<-function(data_transformed, number_genes){
+#computes euclidean distance between gene expression profiles for each gene pair
+comp_euclidean<-function(data, number_genes){
   
   number_genes=10
   distance_array<-numeric(length=number_genes*(number_genes-1)/2)
@@ -77,7 +77,7 @@ comp_euclidean<-function(data_transformed, number_genes){
   pos=1
   for (reference_gene in 1:(number_genes-1)){
     for(gene in (reference_gene+1):number_genes){
-      distance_array[pos]<-sqrt(sum( (data_transformed[reference_gene,][-1] - data_transformed[gene,][-1])^2 ))
+      distance_array[pos]<-sqrt(sum( (data[reference_gene,][-1] - data[gene,][-1])^2 ))
       pos=pos+1
       print(gene)
     }
@@ -87,15 +87,15 @@ comp_euclidean<-function(data_transformed, number_genes){
   
 }
 
-#computes spearman correlation between transformed gene expression profiles for each  gene pair
-comp_spearman<-function(data_transformed, number_genes){
+#computes spearman correlation between gene expression profiles for each gene pair
+comp_spearman<-function(data, number_genes){
   
   distance_array<-numeric(length=number_genes*(number_genes-1)/2)
   
   pos=1
   for (reference_gene in 1:(number_genes-1)){
     for(gene in (reference_gene+1):number_genes){
-      distance_array[pos]<-cor(data_transformed[reference_gene,][-1], data_transformed[gene,][-1], method="spearman")
+      distance_array[pos]<-cor(data[reference_gene,][-1], data[gene,][-1], method="spearman")
       pos=pos+1
       
     }
@@ -105,15 +105,15 @@ comp_spearman<-function(data_transformed, number_genes){
   
 }
 
-#computes pearson correlation between transformed gene expression profiles for each  gene pair
-comp_pearson<-function(data_transformed, number_genes){
+#computes pearson correlation between gene expression profiles for each gene pair
+comp_pearson<-function(data, number_genes){
   
   distance_array<-numeric(length=number_genes*(number_genes-1)/2)
   
   pos=1
   for (reference_gene in 1:(number_genes-1)){
     for(gene in (reference_gene+1):number_genes){
-      distance_array[pos]<-cor(data_transformed[reference_gene,][-1], data_transformed[gene,][-1], method="pearson")
+      distance_array[pos]<-cor(data[reference_gene,][-1], data[gene,][-1], method="pearson")
       pos=pos+1
       
     }
@@ -123,15 +123,15 @@ comp_pearson<-function(data_transformed, number_genes){
   
 }
 
-#computes RIO (Relative intensity overlap) between transformed gene expression profiles for each  gene pair
-comp_rio<-function(data_transformed, number_genes){
+#computes RIO (Relative intensity overlap) between gene expression profiles for eachgene pair
+comp_rio<-function(data, number_genes){
   
   distance_array<-numeric(length=number_genes*(number_genes-1)/2)
   
   pos=1
   for (reference_gene in 1:(number_genes-1)){
     for(gene in (reference_gene+1):number_genes){
-      distance_array[pos]<-rio(data_transformed[reference_gene,][-1], data_transformed[gene,][-1])
+      distance_array[pos]<-rio(data[reference_gene,][-1], data[gene,][-1])
       pos=pos+1
       
     }
@@ -147,15 +147,15 @@ rio<-function(a, b){
     return(rio_ab)
 }
 
-#computes mutual information between transformed gene expression profiles for each  gene pair
-comp_mutual_information<-function(data_transformed, number_genes){
+#computes mutual information between gene expression profiles for each gene pair
+comp_mutual_information<-function(data, number_genes){
   
   distance_array<-numeric(length=number_genes*(number_genes-1)/2)
   
   pos=1
   for (reference_gene in 1:(number_genes-1)){
     for(gene in (reference_gene+1):number_genes){
-      distance_array[pos]<-mutinformation(data_transformed[reference_gene,][-1], data_transformed[gene,][-1], method="emp")
+      distance_array[pos]<-mutinformation(data[reference_gene,][-1], data[gene,][-1], method="emp")
       pos=pos+1
       
     }
@@ -169,17 +169,17 @@ comp_mutual_information<-function(data_transformed, number_genes){
 ################################################################################################
 
 
-#read haar wavelet transformed data
+#read gene expression data
 inputfile<-opt$input
-data_transformed<-fread(inputfile, header=TRUE, sep="\t", data.table=FALSE)
-data_transformed<-as.matrix(data_transformed)
+data<-fread(inputfile, header=TRUE, sep="\t", data.table=FALSE)
+data<-as.matrix(data)
 
 #method to compare gene expression profiles
 distance_measures<-names(which(opt==TRUE))
 #save GENEIDs
-gene_ids<-data_transformed[,1]
+gene_ids<-data[,1]
 #save the number of genes
-number_genes<-nrow(data_transformed)
+number_genes<-nrow(data)
 
 ##create basic name of the output file
 #remove path from the inputfilename
@@ -188,7 +188,7 @@ outputfile<-sub(".*\\/", "", inputfile)
 outputfile<-sub("(\\.[[:alnum:]]+$)", "", outputfile)
 
 #remark for the outputfile
-remark<-"#The following line contains the distance or correlation between the haar wavelet transformed gene expression profiles for each gene pair. Only the upper triangle of the distance/correlation matrix is saved as a vector.\n#Formula to access the distance between the gene expression profile of the i-th and the j-th gene : number_genes*(i-1)+j-i*(i+1)/2"
+remark<-"#The following line contains the distance or correlation between the gene expression profiles for each gene pair. Only the upper triangle of the distance/correlation matrix is saved as a vector.\n#Formula to access the distance between the gene expression profile of the i-th and the j-th gene : number_genes*(i-1)+j-i*(i+1)/2"
 
 
 #calculate distance or correlation between each gene pair
@@ -197,20 +197,20 @@ for (method in distance_measures){
   
   switch(method, 
          euclidean={
-           distance_array<-comp_euclidean(data_transformed, number_genes)
+           distance_array<-comp_euclidean(data, number_genes)
         },
          spearman={
-           distance_array<-comp_spearman(data_transformed, number_genes)
+           distance_array<-comp_spearman(data, number_genes)
            
         },
          pearson={
-           distance_array<-comp_pearson(data_transformed, number_genes)    
+           distance_array<-comp_pearson(data, number_genes)    
         },
         rio={
-           distance_array<-comp_rio(data_transformed, number_genes)   
+           distance_array<-comp_rio(data, number_genes)   
          },
          mutual_information={
-           distance_array<-comp_mutual_information(round(data_transformed), number_genes) #Werte mussten gerundet werden?   
+           distance_array<-comp_mutual_information(round(data), number_genes) #Werte mussten gerundet werden?   
          }
   )
   
