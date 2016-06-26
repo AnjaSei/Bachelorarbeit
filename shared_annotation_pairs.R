7821bd66fec7d42858835b1666aa1e22389b1dbb
AnjaSei <anjaseidel1@gmx.net>
2016-06-26 16:58:09 +0200

    new script to calculate shared annotation pairs

--- shared_annotation_pairs.R
+++ shared_annotation_pairs.R
@@ -2,28 +2,30 @@
 
 ########################################################################
 ##
-## File: shared_annotation.R
+## File: shared_annotation_pairs.R
 ##
 ## Created by: Anja S.
-## Created on: June 12, 2016
+## Created on: June 22, 2016
 ##
-## Description: Determines the number of gene pairs which share an annotation 
-##  in each ranked co-expression group (RCG).
+## Description: Determines the number of gene pairs with shared annotation 
+##  per ranked co-expression group (RCG).
 ##     
 ## Input:   File with the ranked co-expression groups.
 ##
-## Output: File which contains number of gene pairs with shared 
-##  annotation for each ranked co-expression group.
+## Output: File with number of gene pairs with shared annotation for 
+##  different sizes per RCG.
 ##          
 ########################################################################
 
 #install CRAN packages
 #install.packages("optparse")
 #install.packages("data.table")
+#install.packages("schoolmath")
 
 #load packages
 library(optparse) #to parse command line options
 library(data.table)
+library(schoolmath)
 
 #command line options
 option_list <- list(
@@ -64,18 +66,23 @@ colnames(annotation)<-c("GENEID", "ANNOTATION")
 
 #load file with the ranked co-expression groups
 inputfile<-opt$input
-rcg<-fread(inputfile, header=TRUE, sep="\t", data.table=FALSE)
-rcg<-as.matrix(rcg)
-ref_genes<-rcg[,1]
+rcgs<-fread(inputfile, data.table=FALSE, header=TRUE)
+rcgs<-as.matrix(rcgs)
 
-#only around 5000 GeneIDs have an annotation -> set GeneIDs without unknown annotation to NA
-#to save running time (later in the code)
-rcg[which(!(rcg %in% annotation$GENEID))]<-NA
+size_rcg<-max(rcgs[,"rank"])
+geneIDs<-unique(rcgs[,"RCG"])
 
-#number genes per RCG
-size_rcg<-ncol(rcg)
+#build a new matrix: each line represents a RCG with its GeneIDs
+if(is.decimal(nrow(rcgs)/size_rcg)){
+  stop("RCGs have different size!")
+}
+rcgs<-matrix(rcgs[,"gene"], ncol=size_rcg, byrow=TRUE)
 #number of RCGs
-number_rcg<-nrow(rcg)
+number_rcg<-nrow(rcgs)
+
+#only some GeneIDs have an annotation -> set GeneIDs with unknown annotation to NA to save running time
+rcgs[which(!(rcgs %in% annotation$GENEID))]<-NA
+
 
 ##create name of the output file
 #remove path from the RCG-filename
@@ -88,23 +95,28 @@ annotationfilename<-sub(".*\\/", "", annotationfile)
 annotationfilename<-sub("(\\.[[:alpha:]]+$)", "", annotationfilename)
 outputfile<-paste0(output_folder, "/", rcg_filename, "_annotation_", annotationfilename, ".txt")
 
-#saves the number of gene pairs with shared annotation for each RCG
-shared_anno<-numeric(length=number_rcg)
+#k is the number of genes with the most correlated 3D gene expression profile to the reference gene
+all_k<-c(seq_len(5),seq(10, size_rcg-1, 10))
+
+#saves the number of gene pairs with shared annotation for each k per RCG
+shared_anno<-matrix(data=NA, ncol=length(all_k)+1, nrow=number_rcg)
+colnames(shared_anno)<-c("RCG", paste0("k=",all_k))
+shared_anno[,1]<-geneIDs
 
 #for each ranked co-expression group do:
-for(i in 1:number_rcg) {
-  
-  #load GeneIDs for each ranked co-expression group
-  rcg_i <- rcg[i,]
-  
-  #count gene pairs with shared annotation per RCG
+for(rcg in 1:number_rcg) {
+
+  #load GeneIDs
+  rcg_i <- rcgs[rcg,]
+
+  #count gene pairs with shared annotation for each k per RCG 
   counter=0
-  for(gene1 in 1:(size_rcg-1)){
-    for(gene2 in (gene1+1):size_rcg){
+  for(j in 2:size_rcg){
+    for(i in 1:(j-1)){
       
       #select GeneIDs
-      geneID1<-rcg_i[gene1]
-      geneID2<-rcg_i[gene2]
+      geneID1<-rcg_i[i]
+      geneID2<-rcg_i[j]
       
       #both GeneIDs have an annotation 
       if(!is.na(geneID1) && !is.na(geneID2)){
@@ -119,16 +131,20 @@ for(i in 1:number_rcg) {
         }
         
       }
+      
+      #save number of shared gene pairs for a specific k
+      if( (i==j-1) &&  sum(all_k==j-1) ){
+        shared_anno[rcg, which(all_k==j-1)+1]<-counter    
+      }
     }
-    
   }
-  #save the number of gene pairs with shared annotation
-  shared_anno[i]<-counter
 
 }
 
-#save results in the outputfile
-result_matrix<-cbind(ref_genes, shared_anno)
-colnames(result_matrix)<-c("ranked_co_expression_group", "number_shared_annotation_pairs")
-write.table(result_matrix, outputfile, row.names=FALSE, col.names=TRUE, sep="\t", append=FALSE, quote=FALSE)
+#save the results in the outputfile
+comment<-paste0("#File with the ranked co-expression groups: ", inputfile, 
+                "\n#File with the annotation: ", annotationfile,
+                "\n# k=number of most correlated genes to each gene's RCG")
+write.table(comment, outputfile, row.names=FALSE, col.names=FALSE, sep="\t", append=FALSE, quote=FALSE )
+write.table(shared_anno, outputfile, row.names=FALSE, col.names=TRUE, sep="\t", append=TRUE, quote=FALSE)
 
